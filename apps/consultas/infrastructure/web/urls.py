fe681c2d122485128c28f90580b8c816f971e65c
DavidVelaz17 <david.lara.velaz@gmail.com>
2025-10-03 20:56:46 -0600

    [TASK]: Solicitudes IMPI e INDAUTOR #4 - Branch rebased

--- apps/consultas/infrastructure/web/urls.py
+++ apps/consultas/infrastructure/web/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('instituciones/top10/', ConsultaViewSet.as_view({'get': 'instituciones_top10_view'}), name='instituciones-top10'),
     path('sectores/', ConsultaViewSet.as_view({'get': 'registros_por_sector_view'}), name='registros-por-sector'),
     path('investigadores/sexo/', ConsultaViewSet.as_view({'get': 'registros_por_sexo_view'}), name='registros-por-sexo'),
+    path('tableros/requests/impi/', ConsultaViewSet.as_view({'get': 'requests_impi_view'}), name='requests-impi'),
+    path('tableros/requests/indautor/', ConsultaViewSet.as_view({'get': 'requests_indautor_view'}),
+         name='requests-indautor'),
+
+    path("", include(router.urls)),
 ]
