2ad6fcc1d57f5cffa758a46c31082b143f16d384
David Cruz <100491557+Cruz1229@users.noreply.github.com>
2025-10-04 05:59:46 -0600

    feat: Endpoint para conteo de registros por sexo de investigador (#23)

--- apps/consultas/infrastructure/web/serializer.py
+++ apps/consultas/infrastructure/web/serializer.py
@@ -17,3 +17,7 @@ class InstitucionTopSerializer(serializers.Serializer):
     id_institucion = serializers.IntegerField()
     institucion_nombre = serializers.CharField()
     total = serializers.IntegerField()
+
+class RegistrosPorSexoSerializer(serializers.Serializer):
+    sexo = serializers.CharField()
+    total = serializers.IntegerField()
