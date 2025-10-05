482f3fc73ba66b786323e56aacae26071d9881aa
DavidVelaz17 <david.lara.velaz@gmail.com>
2025-10-04 19:26:11 -0600

    Query fixed

--- apps/consultas/infrastructure/repositories/requests_by_type_repo.py
+++ apps/consultas/infrastructure/repositories/requests_by_type_repo.py
@@ -11,12 +11,25 @@ def get_requests_by_type(institucion: str) -> List[Dict]:
     """
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT 
-                p.nombre AS rama,
-                f.total
-            FROM f_cuenta_registros_por_tipo_registro() f
-            JOIN parametrizacion AS p ON f.tipo_registro_param = p.id_param
-            ORDER BY total DESC, p.nombre;
+        SELECT
+            p_tipo.nombre AS tipo_registro,
+            p_rama.nombre AS rama,
+            COUNT(r.id_registro) AS total
+        FROM
+            f_cuenta_registros_por_tipo_registro() AS f
+        JOIN
+            parametrizacion AS p_tipo ON p_tipo.id_param = f.tipo_registro_param
+        JOIN 
+            registro AS r ON r.tipo_registro_param = f.tipo_registro_param
+        JOIN
+            parametrizacion AS p_rama ON p_rama.id_param = r.rama_param
+        WHERE 
+            p_tipo.nombre = %s
+        GROUP BY
+            p_tipo.nombre, 
+            p_rama.nombre
+        ORDER BY
+            total DESC, p_tipo.nombre, p_rama.nombre;
         """, [institucion.upper()])
 
         cols = [c[0] for c in cursor.description]
