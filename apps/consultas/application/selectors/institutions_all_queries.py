32dfce298db965120aca09995b222649bdb1e3a1
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-10-15 19:39:37 -0600

    Revert "Corregir servicios de Inhabilitación/Habilitación de registros (#62)" (#63)
    This reverts commit 5c4cbb6b41618ce5f4f17995e24d00011749b19c.

--- apps/consultas/application/selectors/institutions_all_queries.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Dict
-from apps.consultas.infrastructure.repositories.institutions_all_repo import InstitutionsAllRepository
-
-def instituciones_all() -> List[Dict]:
-    """
-    Selector para obtener todas las instituciones con su total de registros.
-    """
-    repo = InstitutionsAllRepository()
-    return repo.obtener_instituciones_all()
--- apps/registros/application/selectors/enable_record.py
+++
@@ -1,8 +0,0 @@
-from apps.registros.infrastructure.repositories.registros_repo import PostgresRegistroRepository
-
-def enable_record(id_registro: int):
-    """
-    Selector para habilitar un registro.
-    """
-    repo = PostgresRegistroRepository()
-    return repo.habilitar(id_registro)
