7bde87e40744c119f03f61f211bcd732a03cd9f5
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-10-24 15:47:24 -0600

    CRUD CEPATs created (#76)
    Co-authored-by: DavidVelaz17 <david.lara.velaz@gmail.com>

---
+++ apps/cepat/infrastructure/repositories/cepat_repo.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+class CepatSerializer(serializers.Serializer):
+    """
+    Serializador para MOSTRAR la entidad Cepat. (Salida)
+    """
+    id_cepat = serializers.IntegerField(read_only=True)
+    nombre = serializers.CharField(read_only=True)
+
+class CepatInputSerializer(serializers.Serializer):
+    """
+    Serializador para VALIDAR la entrada al crear o actualizar un Cepat. (Entrada)
+    """
+    nombre = serializers.CharField(max_length=255, required=True)
