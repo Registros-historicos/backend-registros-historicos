58b239f2b598cee50120aa39abd22ce82a84cdd2
Braulio David Hernández Palagot <bdpalagot@gmail.com>
2025-11-01 11:15:25 -0600

    Agregar el filtrado para cepat e institución en endpoint de tablero categorías

--- apps/consultas/infrastructure/repositories/category_researchers_repo.py
+++ apps/consultas/infrastructure/repositories/category_researchers_repo.py
@@ -29,3 +29,63 @@ class RegistroRepositorio:
                 dict(zip(columns, row))
                 for row in cursor.fetchall()
             ]
+    
+    def obtener_conteo_global(self) -> list[dict]:
+        """
+        Conteo global para rol 35 - función f_cuenta_registros_por_tipo_investigador
+        """
+        query = """
+            SELECT
+                p.nombre AS categoria,
+                t.total
+            FROM
+                f_cuenta_registros_por_tipo_investigador() AS t
+            INNER JOIN
+                parametrizacion AS p ON t.tipo_investigador_param = p.id_param
+            ORDER BY
+                t.total DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
+    
+    def obtener_conteo_por_cepat(self, id_cepat: int) -> list[dict]:
+        """
+        Conteo por CEPAT para rol 37 - función f_cuenta_registros_por_tipo_investigador_cepat
+        """
+        query = """
+            SELECT
+                p.nombre AS categoria,
+                t.total
+            FROM
+                f_cuenta_registros_por_tipo_investigador_cepat(%s) AS t
+            INNER JOIN
+                parametrizacion AS p ON t.tipo_investigador_param = p.id_param
+            ORDER BY
+                t.total DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_cepat])
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
+    
+    def obtener_conteo_por_institucion(self, id_institucion: int) -> list[dict]:
+        """
+        Conteo por institución para rol 36 - función f_cuenta_registros_por_tipo_investigador_institucion
+        """
+        query = """
+            SELECT
+                p.nombre AS categoria,
+                t.total
+            FROM
+                f_cuenta_registros_por_tipo_investigador_institucion(%s) AS t
+            INNER JOIN
+                parametrizacion AS p ON t.tipo_investigador_param = p.id_param
+            ORDER BY
+                t.total DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_institucion])
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
