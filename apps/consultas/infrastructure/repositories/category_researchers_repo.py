bd8088002b00cff6b2157087559104e7adc65b32
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-11-01 11:45:25 -0600

    Feat tableros categoría y sexo (#104) (#105)
    * Agregar el filtrado para cepat e institución en endpoint de tablero categorías
    
    * Agregar el filtrado para cepat e institución en endpoint de tablero sexo
    
    Co-authored-by: Braulio David Hernández Palagot <bdpalagot@gmail.com>

--- apps/consultas/infrastructure/repositories/category_researchers_repo.py
+++ apps/consultas/infrastructure/repositories/category_researchers_repo.py
@@ -29,3 +29,68 @@ class RegistroRepositorio:
                 dict(zip(columns, row))
                 for row in cursor.fetchall()
             ]
+    
+    def obtener_conteo_global(self) -> list[dict]:
+        """
+        Conteo global para rol 35 - función f_cuenta_registros_por_tipo_investigador
+        """
+        query = """
+        WITH count AS (
+            SELECT tipo_investigador_param, total
+            FROM f_cuenta_registros_por_tipo_investigador()
+        )
+            SELECT
+                p.nombre AS categoria,
+                COALESCE(t.total, 0) AS total
+            FROM parametrizacion AS p
+            LEFT JOIN count AS t ON t.tipo_investigador_param = p.id_param
+            WHERE p.id_tema = 10
+            ORDER BY
+                total DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    def obtener_conteo_por_cepat(self, id_cepat: int) -> list[dict]:
+        """
+        Conteo por CEPAT para rol 37 - función f_cuenta_registros_por_tipo_investigador_cepat
+        """
+        query = """
+        WITH count AS (
+            SELECT tipo_investigador_param, total
+            FROM f_cuenta_registros_por_tipo_investigador_cepat(%s)
+        )
+            SELECT
+                p.nombre AS categoria,
+                COALESCE(t.total, 0) AS total
+            FROM parametrizacion AS p
+            LEFT JOIN count AS t ON t.tipo_investigador_param = p.id_param
+        WHERE p.id_tema = 10
+            ORDER BY
+                total DESC;
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_cepat])
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    def obtener_conteo_por_institucion(self, id_institucion: int) -> list[dict]:
+        """
+        Conteo por institución para rol 36 - función f_cuenta_registros_por_tipo_investigador_institucion
+        """
+        query = """
+                WITH count AS (SELECT tipo_investigador_param, total
+                               FROM f_cuenta_registros_por_tipo_investigador_institucion(%s))
+                SELECT p.nombre             AS categoria,
+                       COALESCE(t.total, 0) AS total
+                FROM parametrizacion AS p
+                         LEFT JOIN count AS t ON t.tipo_investigador_param = p.id_param
+                WHERE p.id_tema = 10
+                ORDER BY total DESC; \
+                """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_institucion])
+            columns = [col[0] for col in cursor.description]
+            return [dict(zip(columns, row)) for row in cursor.fetchall()]
