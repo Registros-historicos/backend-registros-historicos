788bda9ddeb15bb461c987be9aa1c91fb7566cf8
Jonathan Garcia Amador <jonabb810@hotmail.com>
2025-11-03 18:19:30 -0600

    Feature/selector registros por periodo por rol (#123)
    * Agrega filtro por tipo y rol de usuario en selector de registros por periodo (admin, coordinador, cepat)
    
    * se agregos filtro por usuarios en tablero periodo
    
    * extender lógica de filtros para segmentar registros según tipo de usuario

--- apps/consultas/infrastructure/web/views.py
+++ apps/consultas/infrastructure/web/views.py
@@ -188,9 +188,9 @@ class ConsultaViewSet(viewsets.ViewSet):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        resultado = registros_por_mes_selector(anio)
+        resultado = registros_por_mes_selector(anio, request.user)
         return Response(resultado, status=status.HTTP_200_OK)
-    
+
     
     @extend_schema(
         summary="Conteo de registros agrupados por año, mes y tipo de registro",
@@ -235,7 +235,7 @@ class ConsultaViewSet(viewsets.ViewSet):
             )
 
         try:
-            resultado = registros_por_periodo_selector(fecha_inicio, fecha_fin)
+            resultado = registros_por_periodo_selector(fecha_inicio, fecha_fin, request.user)
             serializer = RegistrosPorPeriodoSerializer(resultado, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
