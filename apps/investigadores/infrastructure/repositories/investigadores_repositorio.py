cc9a24ca9405a85bac01afc72c5785cb5f70dd13
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-12-16 17:46:36 -0600

    Develop (#172)
    * Obervaciones atendidas (#163)
    
    Co-authored-by: Cruz <j.ramrez12.j@gmail.com>
    
    * fix carga masiva
    
    * fix carga masiva (#164)
    
    * fix/carga masiva
    
    * chore registros impi e indautor: Filtrado en el listado
    
    * chore docker
    
    * chore docker
    
    * fix indautor bulk
    
    * feat listar curp (#171)
    
    ---------
    
    Co-authored-by: David Cruz <100491557+Cruz1229@users.noreply.github.com>
    Co-authored-by: Cruz <j.ramrez12.j@gmail.com>

---
+++ apps/investigadores/infrastructure/repositories/investigadores_repositorio.py
@@ -0,0 +1,54 @@
+from apps.registros.infrastructure.repositories.pg_utils import run_query
+from .pg_utils import call_function
+
+
+
+class PostgresInvestigadorRepository:
+    """
+    Repositorio PostgreSQL para tabla 'investigador' y relación con 'adscripcion'.
+    """
+
+    def list_all_investigadores(self):
+        """Obtiene todos los investigadores."""
+        query = """
+            SELECT i.id_investigador, i.curp, i.nombre, i.ape_pat, i.ape_mat,
+                   i.sexo_param, i.tipo_investigador_param
+            FROM investigador i;
+        """
+        return run_query(query)
+
+    def list_curps(self):
+
+        """Obtiene todas las CURP de investigadores."""
+        query = "SELECT * FROM obtener_curp();"
+
+        print("Ejecutando query para listar CURPs de investigadores.")
+        print(call_function(query))
+        return call_function(query)
+
+    def get_by_curp(self, curp: str):
+        """Obtiene un investigador por CURP."""
+        query = """
+            SELECT i.id_investigador, i.curp, i.nombre, i.ape_pat, i.ape_mat,
+                   i.sexo_param, i.tipo_investigador_param
+            FROM investigador i
+            WHERE i.curp = %s;
+        """
+        return run_query(query, [curp], fetchone=True)
+
+    def vincular_adscripcion(self, id_investigador: int, adscripcion_data: dict):
+        """Crea una adscripción si no existe una activa (opcional)."""
+        query = """
+            INSERT INTO adscripcion (
+                departamento_param, programa_educativo_param,
+                cuerpo_academico_param, fec_ini, fec_fin,
+                id_institucion, id_investigador
+            )
+            VALUES (
+                %(departamento_param)s, %(programa_educativo_param)s,
+                %(cuerpo_academico_param)s, %(fec_ini)s, %(fec_fin)s,
+                %(id_institucion)s, %(id_investigador)s
+            )
+            ON CONFLICT DO NOTHING;
+        """
+        run_query(query, adscripcion_data)
