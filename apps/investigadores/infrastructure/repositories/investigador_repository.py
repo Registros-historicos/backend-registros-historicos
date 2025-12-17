a332f16585bf88a57498defc5d486f1d5f4f9b8a
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-12-16 22:57:22 -0600

    fix

--- apps/investigadores/infrastructure/repositories/investigador_repository.py
+++ apps/investigadores/infrastructure/repositories/investigador_repository.py
@@ -67,3 +67,14 @@ class InvestigadorRepository(InvestigadorRepositoryPort):
         )
         return result[0] if result else None
 
+
+    def get_by_curp(self, curp: str):
+        """Obtiene un investigador por CURP."""
+        query = """
+            SELECT i.id_investigador, i.curp, i.nombre, i.ape_pat, i.ape_mat,
+                   i.sexo_param, i.tipo_investigador_param
+            FROM investigador i
+            WHERE i.curp = %s;
+        """
+        return run_query(query, [curp], fetchone=True)
+
--- apps/registros/infrastructure/repositories/investigadores_repositorio.py
+++ apps/registros/infrastructure/repositories/investigadores_repositorio.py
@@ -21,3 +21,16 @@ class PostgresInvestigadorRepository:
             ON CONFLICT DO NOTHING;
         """
         run_query(query, adscripcion_data)
+
+
+
+    def get_by_curp(self, curp: str):
+        """Obtiene un investigador por CURP."""
+        query = """
+            SELECT i.id_investigador, i.curp, i.nombre, i.ape_pat, i.ape_mat,
+                   i.sexo_param, i.tipo_investigador_param
+            FROM investigador i
+            WHERE i.curp = %s;
+        """
+        return run_query(query, [curp], fetchone=True)
+
